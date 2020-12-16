bec661419e2e7a56c020e2e99221732e31e1ab57
KolpixTV <kolpix3.0@gmail.com>
2020-12-16 14:43:43 +0100

    Update: sortieren.sh
    Bug fix

---
+++ sortieren.sh
@@ -0,0 +1,280 @@
+echo "Pfad wechseln"
+
+cd ~/Downloads
+
+echo "Berechtigungen einstellen"
+
+sudo chmod 777 ~/Downloads/*
+
+echo "Dateien Erstellen und Ordnen"
+
+mkdir Sonstiges
+
+mv ~/Downloads/* ~/Downloads/Sonstiges
+
+mkdir Videos
+mv ~/Downloads/Sonstiges/Videos ~/Downloads
+
+mkdir Programme
+mv ~/Downloads/Sonstiges/Programme ~/Downloads
+
+mkdir Musik
+mv ~/Downloads/Sonstiges/Musik ~/Downloads
+
+mkdir Dokumente
+mv ~/Downloads/Sonstiges/Dokumente ~/Downloads
+
+mkdir Bilder
+mv ~/Downloads/Sonstiges/Bilder ~/Downloads
+ 
+mkdir Installationsdateien
+mv ~/Downloads/Sonstiges/Installationsdateien ~/Downloads
+
+mkdir Entwicklung
+mv ~/Downloads/Sonstiges/Entwicklung ~/Downloads
+
+mkdir zip rar tar
+mv ~/Downloads/Sonstiges/zip ~/Downloads
+mv ~/Downloads/Sonstiges/rar ~/Downloads
+mv ~/Downloads/Sonstiges/tar ~/Downloads
+
+mv ~/Downloads/Videos/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Programme/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Musik/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Dokumente/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Bilder/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Installationsdateien/* ~/Downloads/Sonstiges/
+mv ~/Downloads/Entwicklung/* ~/Downloads/Sonstiges/
+mv ~/Downloads/zip/* ~/Downloads/Sonstiges/
+mv ~/Downloads/rar/* ~/Downloads/Sonstiges/
+mv ~/Downloads/tar/* ~/Downloads/Sonstiges/
+
+mkdir web
+mv ~/Downloads/Sonstiges/web ~/Downloads
+
+
+echo "Dateien sortieren"
+
+cd ~/Downloads
+
+mv ~/Downloads/Sonstiges/*.py ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.py.* ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.python ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.python.* ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.java ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.java.* ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.javascript ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.javascript.* ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.js ~/Downloads/Entwicklung 
+mv ~/Downloads/Sonstiges/*.js.* ~/Downloads/Entwicklung 
+mv ~/Downloads/Sonstiges/*.json ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.json.* ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.jar ~/Downloads/Entwicklung
+mv ~/Downloads/Sonstiges/*.jar.* ~/Downloads/Entwicklung
+
+mv ~/Downloads/Sonstiges/*.sh ~/Downloads/Programme
+mv ~/Downloads/Sonstiges/*.sh.* ~/Downloads/Programme
+mv ~/Downloads/Sonstiges/*.desktop ~/Downloads/Programme
+mv ~/Downloads/Sonstiges/*.desktop.* ~/Downloads/Programme
+
+
+mv ~/Downloads/Sonstiges/*.html ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.html.* ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.php ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.php.* ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.css ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.css.* ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.xml ~/Downloads/web
+mv ~/Downloads/Sonstiges/*.xml.* ~/Downloads/web
+mv ~/Downloads/Sonstiges/css ~/Downloads/web
+mv ~/Downloads/Sonstiges/img ~/Downloads/web
+mv ~/Downloads/Sonstiges/forum ~/Downloads/web
+mv ~/Downloads/Sonstiges/webseite ~/Downloads/web
+mv ~/Downloads/Sonstiges/Neue-Webseite ~/Downloads/web
+mv ~/Downloads/Sonstiges/js ~/Downloads/web
+
+mv ~/Downloads/Sonstiges/*.tar.gz ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.gz.* ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.xz ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.xz.* ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.* ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.bz ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.bz.* ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.bz2 ~/Downloads/tar
+mv ~/Downloads/Sonstiges/*.tar.bz2.* ~/Downloads/tar
+
+mv ~/Downloads/Sonstiges/*.mp4 ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.mp4.* ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.webm ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.webm.* ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.gif ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.gif.* ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.mpg ~/Downloads/Videos
+mv ~/Downloads/Sonstiges/*.mpg.* ~/Downloads/Videos
+
+mv ~/Downloads/Sonstiges/*.mp3 ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.mp3.* ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.wav ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.wav.* ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.ogg ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.ogg.* ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.midi ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.midi.* ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.mid ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.mid.* ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.m3u ~/Downloads/Musik
+mv ~/Downloads/Sonstiges/*.m3u.* ~/Downloads/Musik
+
+mv ~/Downloads/Sonstiges/*.pdf ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.pdf.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.rft ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.rft.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.docx ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.docx.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.csv ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.csv.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ods ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ods.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.vcf ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.vcf.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.txt ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.txt.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.rtf ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.rtf.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.odt ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.odt.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.doc ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.doc.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.srt ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.srt.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ics ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ics.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.log ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.log.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.lrc ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.lrc.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ini ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.ini.* ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.eml ~/Downloads/Dokumente
+mv ~/Downloads/Sonstiges/*.eml.* ~/Downloads/Dokumente
+
+mv ~/Downloads/Sonstiges/*.jpg ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.* ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.1 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.2 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.3 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.4 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpg.5 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.1 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.* ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.2 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.3 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.4 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.png.5 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.* ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.1 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.2 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.3 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.4 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.jpeg.5 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.* ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.1 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.2 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.3 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.4 ~/Downloads/Bilder
+mv ~/Downloads/Sonstiges/*.webp.5 ~/Downloads/Bilder
+
+mv ~/Downloads/Sonstiges/*.deb ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.exe.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.apk.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.AppImage.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.deb.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.sh.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.run.5 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.* ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.1 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.2 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.3 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.4 ~/Downloads/Installationsdateien
+mv ~/Downloads/Sonstiges/*.dmg.5 ~/Downloads/Installationsdateien
+
+mv ~/Downloads/Sonstiges/*.zip ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.* ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.1 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.2 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.3 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.4 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.zip.5 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.* ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.1 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.2 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.3 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.4 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.sh3p.5 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.* ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.1 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.2 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.3 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.4 ~/Downloads/zip
+mv ~/Downloads/Sonstiges/*.xml.gz.5 ~/Downloads/zip
+
+mv ~/Downloads/Sonstiges/*.rar ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.* ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.1 ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.2 ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.3 ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.4 ~/Downloads/rar
+mv ~/Downloads/Sonstiges/*.rar.5 ~/Downloads/rar
+
+mv ~/Downloads/Programme/sortieren.sh ~/Downloads
+
+echo "Fertig!"
+
+
